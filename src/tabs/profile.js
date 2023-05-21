a980b1cc90149d7e06bb18321ec5e5dcdb50b04a
gabrielemarchetti00 <gabriele.marchetti26@gmail.com>
2023-05-21 13:24:50 +0200

    add display posts on profile page

--- src/tabs/profile.js
+++ src/tabs/profile.js
@@ -1,18 +1,67 @@
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    orderBy,
+    limit,
+    onSnapshot,
+    setDoc,
+    updateDoc,
+    doc,
+    serverTimestamp,
+} from 'firebase/firestore';
+
 export function profileLoad() {
     content.appendChild(profileDiv);
-    displayPosts()
+    loadPosts()
 }
 
-function displayPosts() {
+function loadPosts() {
+    const postsQuery = query(collection(getFirestore(), 'posts'));
+
+    onSnapshot(postsQuery, function(snapshot) {
+        snapshot.docChanges().forEach(function(change) {
+            let post = change.doc.data();
+            displayPost(change.doc.id, post.timestamp, post.user,
+                          post.text, post.profilePicUrl, post.image);
+        });
+      });
+}
+
+function displayPost(id, timestamp, user, text, picUrl, image) {
+    const postDiv = document.getElementById(id);
+    if(postDiv) {
+        const pic = postDiv.querySelector('img');
+        pic.src = image;
+        const desc = postDiv.querySelector('div');
+        desc.textContent = text;
+    }
+    else {
+        createPost(id, image, text);
+    }
+}
+
+function createPost(id, image, text) {
+    const postDiv = document.createElement('div');
+    postDiv.id = id;
+    postDiv.className = 'post-div';
+    profileDiv.appendChild(postDiv);
+
+    const pic = document.createElement('img');
+    pic.src = image;
+    const desc = document.createElement('div');
+    desc.textContent = text;
+    postDiv.appendChild(pic)
+    postDiv.appendChild(desc)
 }
 
 const content = document.querySelector('#content');
 const profileDiv = document.createElement('div');
+profileDiv.id = 'profile-div'
 profileDiv.textContent = "Your Posts";
 
-const postDiv = document.createElement('div');
-const picImg = document.createElement('img');
-const descDiv = document.createElement('div');
+
 
 
 
